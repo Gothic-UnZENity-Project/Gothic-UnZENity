e09011322fbf012c915b7cd332e14ae2701f470c
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2024-06-28 13:59:23 +0200

    add(HVRFocus): Objects (currently ocItems) are now brightened up when focused with hand in HVR.

--- Assets/Gothic-UnZENity-Core/Shaders/Lit-SingleMesh.shader
+++ Assets/Gothic-UnZENity-Core/Shaders/Lit-SingleMesh.shader
@@ -3,6 +3,7 @@ Shader "Lit/SingleMesh"
     Properties
     {
         _MainTex("Texture", 2D) = "white" {}
+        _FocusBrightness("FocusBrightness", float) = 1
     }
     SubShader
     {
@@ -16,6 +17,8 @@ Shader "Lit/SingleMesh"
 
             #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
             #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+
+            float _FocusBrightness;
 
             struct appdata
             {
@@ -74,8 +77,8 @@ Shader "Lit/SingleMesh"
             half4 frag(v2f i) : SV_Target
             {
                 half4 albedo = tex2D(_MainTex, i.uv);
-                half3 diffuse = albedo * i.diffuse;
-                
+                half3 diffuse = albedo * i.diffuse * _FocusBrightness;
+
                 diffuse = ApplyFog(diffuse, i.worldPos);
                 return half4(diffuse, 1);
             }
--- Assets/Gothic-UnZENity-HVR/Scripts/Components/HVRFocus.cs
+++ Assets/Gothic-UnZENity-HVR/Scripts/Components/HVRFocus.cs
@@ -1,19 +1,46 @@
+using GUZ.Core.Globals;
 using HurricaneVR.Framework.Core;
 using HurricaneVR.Framework.Core.Grabbers;
 using UnityEngine;
 
-namespace GVR
+namespace GVR.HVR.Components
 {
     public class HVRFocus : MonoBehaviour
     {
+        private static readonly int _focusBrightness = Shader.PropertyToID("_FocusBrightness");
+
+        private Material _defaultMaterial;
+        private Material _focusedMaterial;
+
         public void OnHoverEnter(HVRGrabberBase grabber, HVRGrabbable grabbable)
         {
+            if (_defaultMaterial == null)
+            {
+                _defaultMaterial = transform.GetComponentInChildren<Renderer>().sharedMaterial;
+                _focusedMaterial = new Material(_defaultMaterial);
+                _focusedMaterial.SetFloat(_focusBrightness, Constants.ShaderPropertyFocusBrightness);
+            }
 
+            transform.GetComponentInChildren<Renderer>().sharedMaterial = _focusedMaterial;
         }
 
         public void OnHoverExit(HVRGrabberBase grabber, HVRGrabbable grabbable)
         {
+            transform.GetComponentInChildren<Renderer>().sharedMaterial = _defaultMaterial;
+        }
 
+        /// <summary>
+        /// Reset everything (e.g. when GO is culled out.)
+        /// </summary>
+        private void OnDisable()
+        {
+            transform.GetComponentInChildren<Renderer>().sharedMaterial = _defaultMaterial;
+
+            // We need to destroy our Material manually otherwise it won't be GC'ed by Unity (as stated in the docs).
+            Destroy(_focusedMaterial);
+
+            _defaultMaterial = null;
+            _focusedMaterial = null;
         }
     }
 }
