e142652c2bc74f99ce6f67879a1b98341496a29c
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2024-06-30 07:39:08 +0200

    add(HVRFocus): Hover focus is now applied to items. Therefore Hover events are registered within oCItem.prefab.

--- Assets/Gothic-UnZENity-HVR/Scripts/Components/HVRFocus.cs
+++ Assets/Gothic-UnZENity-HVR/Scripts/Components/HVRFocus.cs
@@ -1,6 +1,8 @@
 using GUZ.Core.Globals;
+using GUZ.Core.Properties;
 using HurricaneVR.Framework.Core;
 using HurricaneVR.Framework.Core.Grabbers;
+using TMPro;
 using UnityEngine;
 
 namespace GVR.HVR.Components
@@ -8,9 +10,21 @@ namespace GVR.HVR.Components
     public class HVRFocus : MonoBehaviour
     {
         private static readonly int _focusBrightness = Shader.PropertyToID("_FocusBrightness");
+        private static Camera _mainCamera;
+
+        [SerializeField] private VobProperties _properties;
+        [SerializeField] private GameObject _nameCanvas;
 
         private Material _defaultMaterial;
         private Material _focusedMaterial;
+
+        private bool _isHovered;
+
+        private void Start()
+        {
+            _mainCamera = Camera.main;
+            _nameCanvas.SetActive(false);
+        }
 
         public void OnHoverEnter(HVRGrabberBase grabber, HVRGrabbable grabbable)
         {
@@ -20,13 +34,30 @@ namespace GVR.HVR.Components
                 _focusedMaterial = new Material(_defaultMaterial);
                 _focusedMaterial.SetFloat(_focusBrightness, Constants.ShaderPropertyFocusBrightness);
             }
-
             transform.GetComponentInChildren<Renderer>().sharedMaterial = _focusedMaterial;
+
+            _nameCanvas.SetActive(true);
+            _nameCanvas.GetComponentInChildren<TMP_Text>().text = _properties.name; // FIXME - Needs to be altered to the language agnostic name.
+            _isHovered = true;
         }
 
         public void OnHoverExit(HVRGrabberBase grabber, HVRGrabbable grabbable)
         {
             transform.GetComponentInChildren<Renderer>().sharedMaterial = _defaultMaterial;
+
+            _nameCanvas.SetActive(false);
+            _isHovered = false;
+        }
+
+        private void LateUpdate()
+        {
+            if (!_isHovered)
+            {
+                return;
+            }
+
+            _nameCanvas.transform.LookAt(_mainCamera.transform);
+            _nameCanvas.transform.Rotate(0, 180, 0);
         }
 
         /// <summary>
@@ -41,6 +72,7 @@ namespace GVR.HVR.Components
 
             _defaultMaterial = null;
             _focusedMaterial = null;
+            _isHovered = false;
         }
     }
 }
