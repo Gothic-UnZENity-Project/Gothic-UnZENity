9af7d0dc9379470d2409c261d97ceca9cf76ec73
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2024-07-18 14:43:23 +0200

    fix(VobSoundCullingManager): Whenever we spawn at a certain location, the Culling (SetActive(true)) is applied correctly right from the start.

--- Assets/Gothic-UnZENity-Core/Scripts/Manager/Culling/VobSoundCullingManager.cs
+++ Assets/Gothic-UnZENity-Core/Scripts/Manager/Culling/VobSoundCullingManager.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Linq;
-using GUZ.Core.Caches;
 using JetBrains.Annotations;
 using UnityEngine;
 
@@ -38,13 +37,8 @@ namespace GUZ.Core.Manager.Culling
 
         private void SoundChanged(CullingGroupEvent evt)
         {
-            // Ignore Frustum and Occlusion culling.
-            if (evt.previousDistance == evt.currentDistance)
-            {
-                return;
-            }
-
-            var inAudibleRange = evt.previousDistance > evt.currentDistance;
+            // A higher distance level means "inaudible" as we only leverage: 0 -> in-range; 1 -> out-of-range.
+            var inAudibleRange = evt.currentDistance == 0;
 
             _objects[evt.index].SetActive(inAudibleRange);
         }
@@ -83,23 +77,6 @@ namespace GUZ.Core.Manager.Culling
         /// </summary>
         public void PostWorldCreate(GameObject playerGo)
         {
-            /*
-             * We need to check for all Sounds once, if they need to be activated as they're next to player.
-             * As CullingGroup only triggers deactivation once player spawns, but no activation.
-             */
-            var loc = Camera.main!.transform.position;
-            foreach (var sound in LookupCache.VobSoundsAndDayTime.Where(i => i != null))
-            {
-                var soundLoc = sound.transform.position;
-                var soundDist = sound.GetComponent<AudioSource>().maxDistance;
-                var dist = Vector3.Distance(loc, soundLoc);
-
-                if (dist < soundDist)
-                {
-                    sound.SetActive(true);
-                }
-            }
-
             var mainCamera = Camera.main!;
             _soundCullingGroup.targetCamera = mainCamera;
             _soundCullingGroup.SetDistanceReferencePoint(mainCamera.transform);
