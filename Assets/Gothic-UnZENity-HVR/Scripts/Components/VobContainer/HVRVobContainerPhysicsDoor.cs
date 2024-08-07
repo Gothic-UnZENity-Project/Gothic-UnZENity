1d0457f2e7a54e4713c885b9bc35c04c4cb8e0bd
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2024-08-07 08:06:39 +0200

    add(HVRVobContainerPHysicsDoor): HVRPhysicsDoor is overwritten to enhance functionality (currently: Hide/Show inventory when opened/closed).

---
+++ Assets/Gothic-UnZENity-HVR/Scripts/Components/VobContainer/HVRVobContainerPhysicsDoor.cs
@@ -0,0 +1,55 @@
+﻿using HurricaneVR.Framework.Components;
+using MyBox;
+using UnityEngine;
+
+namespace GUZ.HVR.Components.VobContainer
+{
+    public class HVRVobContainerPhysicsDoor : HVRPhysicsDoor
+    {
+        [Separator("GUZ - Settings")]
+        [SerializeField] private GameObject _inventory;
+        
+        // Flags to ensure we always call On*() once, everytime open/close status changes.
+        private bool _onOpenedHandled;
+        private bool _onClosedHandled;
+
+        public override void Start()
+        {
+            base.Start();
+            
+            _inventory.SetActive(false);
+        }
+        
+        protected override void Update()
+        {
+            base.Update();
+
+            if (Opened && !_onOpenedHandled)
+            {
+                OnOpened();
+            }
+
+            if (Closed && !_onClosedHandled)
+            {
+                OnClosed();
+            }
+        }
+        
+        private void OnOpened()
+        {
+            // Render items if opened for the first time.
+            _inventory.SetActive(true);
+            
+            _onOpenedHandled = true;
+            _onClosedHandled = false;
+        }
+        
+        private void OnClosed()
+        {
+            _inventory.SetActive(false);
+            
+            _onOpenedHandled = false;
+            _onClosedHandled = true;
+        }
+    }
+}
