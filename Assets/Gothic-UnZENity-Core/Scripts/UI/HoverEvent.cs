44990078687e59d44eb49a74656692eac68475db
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2024-08-07 23:20:36 +0200

    update(MainMenu.prefab): Added OnHoverEnter + OnHoverExit events to brighten up text on main menu for better UX on where the HVR pointer points towards.

---
+++ Assets/Gothic-UnZENity-Core/Scripts/UI/HoverEvent.cs
@@ -0,0 +1,49 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace GUZ.Core.UI
+{
+    public class HoverEvent : MonoBehaviour
+    {
+        public void OnPointerEnter(BaseEventData evt)
+        {
+            if (evt is not PointerEventData pointerEventData)
+            {
+                return;
+            }
+
+            foreach (var hoveredObj in pointerEventData.hovered)
+            {
+                var textComponent = hoveredObj.GetComponent<TMP_Text>();
+
+                if (textComponent == null)
+                {
+                    continue;
+                }
+                
+                textComponent.spriteAsset = GameGlobals.Font.HighlightSpriteAsset;
+            }
+        }
+
+        public void OnPointerExit(BaseEventData evt)
+        {
+            if (evt is not PointerEventData pointerEventData)
+            {
+                return;
+            }
+
+            foreach (var hoveredObj in pointerEventData.hovered)
+            {
+                var textComponent = hoveredObj.GetComponent<TMP_Text>();
+
+                if (textComponent == null)
+                {
+                    continue;
+                }
+
+                textComponent.spriteAsset = GameGlobals.Font.DefaultSpriteAsset;
+            }
+        }
+    }
+}
