2170df7499e1948b5f181790fecdd00480424dac
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2024-08-11 16:35:56 +0200

    update(HVRIntroduceChapter): Title and Subtitle are now displayed.

--- Assets/Gothic-UnZENity-HVR/Scripts/Components/HVRIntroduceChapter.cs
+++ Assets/Gothic-UnZENity-HVR/Scripts/Components/HVRIntroduceChapter.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+﻿using GUZ.Core;
+using GUZ.Core.Creator.Sounds;
+using GUZ.Core.Extensions;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace GUZ.HVR.Components
 {
@@ -7,10 +12,38 @@ namespace GUZ.HVR.Components
     /// </summary>
     public class HVRIntroduceChapter : MonoBehaviour
     {
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private Canvas _chapterCanvas;
+        [SerializeField] private Image _chapterImage;
+        [SerializeField] private TMP_Text _chapterTitle;
+        [SerializeField] private TMP_Text _chapterSubtitle;
+        
+
         public void DisplayIntroduction(string chapter, string text, string texture, string wav, int time)
         {
-            Debug.Log("Introduce called!");
+            PlayAudio(wav);
+            ShowCover(chapter, text, texture, time);
         }
 
+        private void PlayAudio(string wav)
+        {
+            var soundData = ResourceLoader.TryGetSound(wav);
+            _audioSource.clip = SoundCreator.ToAudioClip(soundData);
+            _audioSource.Play();
+        }
+
+        private void ShowCover(string chapter, string text, string texture, int time)
+        {
+            // Set canvas to follow player as world space overlay.
+            _chapterCanvas.worldCamera = Camera.main;
+            _chapterCanvas.planeDistance = 1;
+            
+            // Set texture for cover
+            _chapterImage.material = GameGlobals.Textures.GetEmptyMaterial(MaterialExtension.BlendMode.Opaque);
+            GameGlobals.Textures.SetTexture(texture, _chapterImage.material);
+
+            _chapterTitle.text = chapter;
+            _chapterSubtitle.text = text;
+        }
     }
 }
