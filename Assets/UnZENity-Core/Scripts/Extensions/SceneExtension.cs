819383a4c79053480742ae2c51deafe36eade57d
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2024-09-19 22:06:51 +0200

    update(BootstrapSceneManager): Add initial tasks.

---
+++ Assets/UnZENity-Core/Scripts/Extensions/SceneExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using JetBrains.Annotations;
+using UnityEngine.SceneManagement;
+
+namespace GUZ.Core.Extensions
+{
+    public static class SceneExtension
+    {
+        [CanBeNull]
+        public static T GetComponentInChildren<T>(this Scene scene)
+        {
+            return scene.GetRootGameObjects().Select(i => i.GetComponentInChildren<T>()).FirstOrDefault(i => i != null);
+        }
+    }
+}
--- Assets/UnZENity-Core/Scripts/Manager/Scenes/BootstrapSceneManager.cs
+++ Assets/UnZENity-Core/Scripts/Manager/Scenes/BootstrapSceneManager.cs
@@ -9,13 +9,31 @@ namespace GUZ.Core.Manager.Scenes
     {
 
         [SerializeField] private AudioSource _music;
-        
-        
+
+
         private void Start()
         {
-            _music.volume = PlayerPrefsManager.MusicVolume;
+            StartCoroutine(BootUnity());
             
+            _music.volume = PlayerPrefsManager.MusicVolume;
             GlobalEventDispatcher.PlayerPrefUpdated.AddListener(OnPlayerPrefUpdated);
+        }
+        
+        private IEnumerator BootUnity()
+        {
+            var bootstrapScene = SceneManager.GetSceneByName(Constants.SceneBootstrap);
+            
+            // Check if other scenes are active and remove them now (We need to have a clean Bootstrap scene at start time only).
+            while (SceneManager.loadedSceneCount != 1)
+            {
+                var nextSceneIndex = SceneManager.GetSceneAt(0) == bootstrapScene ? 1 : 0;
+                var nextScene = SceneManager.GetSceneAt(nextSceneIndex);
+                SceneManager.UnloadSceneAsync(nextScene);
+                yield return null;
+            }
+            
+            // Load Player scene
+            SceneManager.LoadSceneAsync(Constants.ScenePlayer, LoadSceneMode.Additive);
         }
         
         private void OnPlayerPrefUpdated(string key, object value)
