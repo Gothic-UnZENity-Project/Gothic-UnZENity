869e6843c73fdaa7be9f20c0c51cbd8c2b72678e
BoroBongo <borobongo@gmail.com>
2024-10-09 12:42:06 +0100

    added a isPlayerInvincible check for dialogbox

--- Assets/UnZENity-VR/Scripts/Components/VRNpc.cs
+++ Assets/UnZENity-VR/Scripts/Components/VRNpc.cs
@@ -1,4 +1,5 @@
 ﻿#if GUZ_HVR_INSTALLED
+using GUZ.Core.Caches;
 using GUZ.Core.Globals;
 using GUZ.Core.Manager;
 using GUZ.Core.Properties;
@@ -12,14 +13,19 @@ namespace GUZ.VR.Components
     {
         public void OnGrabbed(HVRGrabberBase grabber, HVRGrabbable grabbable)
         {
-            if (GameData.Dialogs.IsInDialog)
+            var foo = MultiTypeCache.NpcCache[GameData.GothicVm.GlobalOther.Index];
+            var isPlayerInvincible = foo.instance.GetAiVar(Constants.DaedalusConst.AIVInvincibleKey);
+            Debug.Log($"NPC Grabbes: IsInDialog = {GameData.Dialogs.IsInDialog}, AIVInvincible = {isPlayerInvincible}");
+            if (GameData.Dialogs.IsInDialog && isPlayerInvincible == 1)
             {
                 DialogManager.SkipCurrentDialogLine(GetComponent<NpcProperties>());
+                Debug.Log("Skipping current dialog line");
             }
             else
             {
                 // FIXME - We need to call passive Perception Perc_ASSESSTALK rather than starting the dialog this way.
                 DialogManager.StartDialog(gameObject, GetComponent<NpcProperties>(), true);
+                Debug.Log("Starting dialog");
             }
         }
     }
