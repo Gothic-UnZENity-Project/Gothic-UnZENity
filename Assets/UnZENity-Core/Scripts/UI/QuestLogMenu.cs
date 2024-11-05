15833d8c4433310a988f4a19e0137382c77d47b6
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2024-11-05 05:54:44 +0100

    feat(QuestLog): Menu entries on the left are added dynamically.

--- Assets/UnZENity-Core/Scripts/UI/QuestLogMenu.cs
+++ Assets/UnZENity-Core/Scripts/UI/QuestLogMenu.cs
@@ -1,4 +1,6 @@
 ﻿using GUZ.Core.Globals;
+using MyBox;
+using TMPro;
 using UnityEngine;
 using ZenKit.Daedalus;
 
@@ -6,6 +8,9 @@ namespace GUZ.Core.UI
 {
     public class QuestLogMenu : MonoBehaviour
     {
+        [SerializeField] private GameObject _canvas;
+        // Menu entries (e.g. text:Current missions) are created dynamically. We therefore use this GO as reference (kind of Prefab).
+        [SerializeField] private GameObject _itemTemplate;
         [SerializeField] private GameObject _background;
 
         private void Start()
@@ -17,6 +22,31 @@ namespace GUZ.Core.UI
         {
             var menuInstance = GameData.MenuVm.InitInstance<MenuInstance>("MENU_LOG");
             _background.GetComponent<MeshRenderer>().material = GameGlobals.Textures.GetMaterial(menuInstance.BackPic);
+
+            for (var i = 0; ; i++)
+            {
+                var menuItemName = menuInstance.GetItem(i);
+
+                // We passed the last item.
+                if (menuItemName.IsNullOrEmpty())
+                {
+                    break;
+                }
+
+                LoadMenuItem(menuItemName);
+            }
+        }
+
+        private void LoadMenuItem(string menuItemName)
+        {
+            var item = GameData.MenuVm.InitInstance<MenuItemInstance>(menuItemName);
+
+            var itemGo = Instantiate(_itemTemplate, _canvas.transform, false);
+            itemGo.name = menuItemName;
+            itemGo.SetActive(true);
+
+            itemGo.transform.localPosition = new Vector2(item.PosX / 100, item.PosY / 100);
+            itemGo.GetComponentInChildren<TMP_Text>().text = item.GetText(0);
         }
 
         public void ToggleVisibility()
