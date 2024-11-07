19e583bcc0f26cbf175eaad150c6e64c0aea4530
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2024-11-07 08:19:00 +0100

    test(QuestLog): Add example missions and notes for Lab.

--- Assets/UnZENity-Lab/Scripts/LabBootstrapper.cs
+++ Assets/UnZENity-Lab/Scripts/LabBootstrapper.cs
@@ -11,6 +11,7 @@ using GUZ.Core.World;
 using GUZ.Lab.Handler;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using ZenKit;
 using Logger = ZenKit.Logger;
 
 namespace GUZ.Lab
@@ -34,11 +35,13 @@ namespace GUZ.Lab
         private VideoManager _videoManager;
         private RoutineManager _npcRoutineManager;
         private GameSettings _settings;
+        private SaveGameManager _save;
         private TextureManager _textureManager;
         private FontManager _fontManager;
+        private StoryManager _story;
 
         public GameSettings Settings => _settings;
-        public SaveGameManager SaveGame => null;
+        public SaveGameManager SaveGame => _save;
         public LoadingManager Loading => null;
         public PlayerManager Player => null;
         public SkyManager Sky => null;
@@ -50,7 +53,7 @@ namespace GUZ.Lab
         public VobMeshCullingManager VobMeshCulling => null;
         public NpcMeshCullingManager NpcMeshCulling => null;
         public VobSoundCullingManager SoundCulling => null;
-        public StoryManager Story => null;
+        public StoryManager Story => _story;
         public VideoManager Video => _videoManager;
 
 
@@ -77,6 +80,8 @@ namespace GUZ.Lab
             Logger.Set(Config.ZenKitLogLevel, Logging.OnZenKitLogMessage);
             DirectMusic.Logger.Set(Config.DirectMusicLogLevel, Logging.OnDirectMusicLogMessage);
             _settings = GameSettings.Load(Config.GameVersion);
+            _save = new SaveGameManager();
+            _story = new StoryManager(Config);
             _textureManager = GetComponent<TextureManager>();
             _fontManager = GetComponent<FontManager>();
             _npcRoutineManager = new RoutineManager(Config);
@@ -92,6 +97,7 @@ namespace GUZ.Lab
             _npcRoutineManager.Init();
             _videoManager.Init();
             _textureManager.Init();
+            _save.LoadNewGame();
         }
 
         private void InitLab()
@@ -116,6 +122,96 @@ namespace GUZ.Lab
             LabLockHandler.Bootstrap();
 
             GameContext.InteractionAdapter.InitUIInteraction(); // For (e.g.) QuestLog to enable hand pointer.
+            BootstrapPlayer();
+        }
+
+        private void BootstrapPlayer()
+        {
+            // Add Missions and Notes
+            {
+                _story.ExtLogCreateTopic("Mission number 1", SaveTopicSection.Missions);
+                {
+                    _story.ExtLogSetTopicStatus("Mission number 1", SaveTopicStatus.Running);
+                    _story.ExtLogAddEntry("Mission number 1", "Mission number 1 entry 1");
+                    _story.ExtLogAddEntry("Mission number 1", "Mission number 1 entry 2");
+                    _story.ExtLogAddEntry("Mission number 1", "Mission number 1 entry 3");
+                }
+                _story.ExtLogCreateTopic("Mission number 2", SaveTopicSection.Missions);
+                {
+                    _story.ExtLogSetTopicStatus("Mission number 2", SaveTopicStatus.Running);
+                    _story.ExtLogAddEntry("Mission number 2", "Mission number 2 entry 1");
+                    _story.ExtLogAddEntry("Mission number 2", "Mission number 2 entry 2");
+                    _story.ExtLogAddEntry("Mission number 2", "Mission number 2 entry 3");
+                }
+                _story.ExtLogCreateTopic("Mission number 3", SaveTopicSection.Missions);
+                {
+                    _story.ExtLogSetTopicStatus("Mission number 3", SaveTopicStatus.Running);
+                    _story.ExtLogAddEntry("Mission number 3", "Mission number 3 entry 1");
+                    _story.ExtLogAddEntry("Mission number 3", "Mission number 3 entry 2");
+                    _story.ExtLogAddEntry("Mission number 3", "Mission number 3 entry 3");
+                }
+                _story.ExtLogCreateTopic("Mission number 4", SaveTopicSection.Missions);
+                {
+                    _story.ExtLogSetTopicStatus("Mission number 4", SaveTopicStatus.Failure);
+                    _story.ExtLogAddEntry("Mission number 4", "Mission number 4 entry 1");
+                    _story.ExtLogAddEntry("Mission number 4", "Mission number 4 entry 2");
+                    _story.ExtLogAddEntry("Mission number 4", "Mission number 4 entry 3");
+                }
+                _story.ExtLogCreateTopic("Mission number 5", SaveTopicSection.Missions);
+                {
+                    _story.ExtLogSetTopicStatus("Mission number 5", SaveTopicStatus.Failure);
+                    _story.ExtLogAddEntry("Mission number 5", "Mission number 5 entry 1");
+                    _story.ExtLogAddEntry("Mission number 5", "Mission number 5 entry 2");
+                    _story.ExtLogAddEntry("Mission number 5", "Mission number 5 entry 3");
+                }
+                _story.ExtLogCreateTopic("Note number 1", SaveTopicSection.Notes);
+                {
+                    _story.ExtLogAddEntry("Note number 1", "Note number 1 entry 1");
+                    _story.ExtLogAddEntry("Note number 1", "Note number 1 entry 2");
+                    _story.ExtLogAddEntry("Note number 1", "Note number 1 entry 3");
+                    _story.ExtLogAddEntry("Note number 1", "Note number 1 entry 4");
+                    _story.ExtLogAddEntry("Note number 1", "Note number 1 entry 5");
+                }
+                _story.ExtLogCreateTopic("Note number 2", SaveTopicSection.Notes);
+                {
+                    _story.ExtLogAddEntry("Note number 2", "Note number 2 entry 1");
+                    _story.ExtLogAddEntry("Note number 2", "Note number 2 entry 2");
+                    _story.ExtLogAddEntry("Note number 2", "Note number 2 entry 3");
+                }
+                _story.ExtLogCreateTopic("Note number 3", SaveTopicSection.Notes);
+                {
+                    _story.ExtLogAddEntry("Note number 3", "Note number 3 entry 1");
+                    _story.ExtLogAddEntry("Note number 3", "Note number 3 entry 2");
+                }
+                _story.ExtLogCreateTopic("Note number 4", SaveTopicSection.Notes);
+                {
+                    _story.ExtLogAddEntry("Note number 4", "Note number 4 entry 1");
+                }
+                _story.ExtLogCreateTopic("Note number 5", SaveTopicSection.Notes);
+                {
+                    _story.ExtLogAddEntry("Note number 5", "Note number 5 entry 1");
+                }
+                _story.ExtLogCreateTopic("Note number 6", SaveTopicSection.Notes);
+                {
+                    _story.ExtLogAddEntry("Note number 6", "Note number 6 entry 1");
+                }
+                _story.ExtLogCreateTopic("Note number 7", SaveTopicSection.Notes);
+                {
+                    _story.ExtLogAddEntry("Note number 7", "Note number 7 entry 1");
+                }
+                _story.ExtLogCreateTopic("Note number 8", SaveTopicSection.Notes);
+                {
+                    _story.ExtLogAddEntry("Note number 8", "Note number 8 entry 1");
+                }
+                _story.ExtLogCreateTopic("Note number 9", SaveTopicSection.Notes);
+                {
+                    _story.ExtLogAddEntry("Note number 9", "Note number 9 entry 1");
+                }
+                _story.ExtLogCreateTopic("Note number 10", SaveTopicSection.Notes);
+                {
+                    _story.ExtLogAddEntry("Note number 10", "Note number 10 entry 1");
+                }
+            }
         }
 
         private void OnDestroy()
