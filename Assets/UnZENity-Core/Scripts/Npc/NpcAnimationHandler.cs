e7f78fb80d9293b0799b8c822cd7966c7b96d7e1
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2025-03-01 15:08:30 +0100

    fix: LookAtNpc is now rotating the head towards Player each frame. (The AnimationAction itself is simply activating this look behaviour)

--- Assets/UnZENity-Core/Scripts/Npc/NpcAnimationHandler.cs
+++ Assets/UnZENity-Core/Scripts/Npc/NpcAnimationHandler.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using GUZ.Core._Npc2;
 using GUZ.Core.Vm;
 using JetBrains.Annotations;
 using MyBox;
@@ -24,6 +26,53 @@ namespace GUZ.Core.Npc
         private void Start()
         {
             StartCoroutine(BlendOutCoroutine());
+        }
+
+        public void StartLookAt(Transform destinationTransform)
+        {
+            _doLookAtNpc = true;
+            _destTransform = destinationTransform;
+        }
+
+        private bool _doLookAtNpc;
+        private Transform _destTransform;
+
+        private const float _headLookDegreeMax = 80f;
+
+        private void LateUpdate()
+        {
+            if (!_doLookAtNpc)
+            {
+                return;
+            }
+
+            var lookRotationVector = _destTransform.position - PrefabProps.Head.position;
+            var lookRotation = Quaternion.LookRotation(lookRotationVector);
+            var currentHeadZ = PrefabProps.Head.rotation.eulerAngles.z;
+            var targetEuler = lookRotation.eulerAngles;
+            var bodyEuler = PrefabProps.Bip01.transform.rotation.eulerAngles;
+
+            var targetXRot = targetEuler.x;
+            // if (targetXRot > 180f)
+            // {
+            //     targetXRot -= 360f; // Convert to -180 to 180 range.
+            // }
+            // targetXRot = Mathf.Clamp(targetXRot, -_headLookDegreeMax, _headLookDegreeMax);
+
+            var targetYRot = targetEuler.y;
+            // targetYRot = (targetYRot + 360f) % 360f;
+            // targetYRot = Mathf.Min(targetYRot, _headLookDegreeMax) + bodyEuler.y;
+            // if (targetYRot > 180f)
+            // {
+            //     targetYRot -= 360f; // Convert to -180 to 180 range.
+            // }
+            // targetYRot = Mathf.Clamp(targetYRot, -_headLookDegreeMax, _headLookDegreeMax);
+
+            var finalEuler = new Vector3(targetXRot, targetYRot, currentHeadZ);
+            var finalRotation = Quaternion.Euler(finalEuler);
+            var newHeadRot = Quaternion.Slerp(PrefabProps.Head.rotation, finalRotation, Time.deltaTime * 200);
+
+            PrefabProps.Head.rotation = newHeadRot;
         }
 
         public void PlayAnimationRepeatedly(string animName)
