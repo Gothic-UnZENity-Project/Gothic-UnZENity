4dbcdcf4ca3af13a703dee93a18315f62c235f1d
Jucan Andrei Daniel <setrofex@gmail.com>
2025-04-12 21:12:21 +0300

    fix(AbstractMenu): properly set font size fix for day and time in QuestLogMenu

--- Assets/UnZENity-Core/Scripts/UI/AbstractMenu.cs
+++ Assets/UnZENity-Core/Scripts/UI/AbstractMenu.cs
@@ -265,7 +265,7 @@ namespace GUZ.Core.UnZENity_Core.Scripts.UI
 
             textComp.text = item.GetText(0);
             textComp.spriteAsset = GameGlobals.Font.TryGetFont(item.FontName);
-            textComp.fontSize = item.FontName.ToLowerInvariant() == "font_old_10_white.tga" ? 18 : 36;
+            textComp.fontSize = item.FontName.ToLowerInvariant().Contains("font_old_10_white") ? 16 : 36;
 
             // Text component needs to align in dimensions with parent rect.
             var textRect = textComp.GetComponent<RectTransform>();
