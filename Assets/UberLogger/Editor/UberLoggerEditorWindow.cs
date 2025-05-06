200fc4f2fe38048c38480b6b489dd13999bc9e31
Jucan Andrei Daniel <setrofex@gmail.com>
2025-05-06 20:16:10 +0300

    fix(UberLoggerEditorWindow): make it editor exclusive Makes the game compilable again

--- Assets/UberLogger/Editor/UberLoggerEditorWindow.cs
+++ Assets/UberLogger/Editor/UberLoggerEditorWindow.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
@@ -1081,3 +1082,4 @@ public class UberLoggerEditorWindow : EditorWindow, UberLoggerEditor.ILoggerWind
         }
     }
 }
+#endif
