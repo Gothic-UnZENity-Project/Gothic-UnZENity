bd9a172b66478bbb1c8525b013a5a18820848434
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2025-05-23 22:24:44 +0200

    fix: Re-enable logging on release builds via UberLogger and new LogCat filter.

--- Assets/UnZENity-Core/Scripts/Util/FileLoggingHandler.cs
+++ Assets/UnZENity-Core/Scripts/Util/FileLoggingHandler.cs
@@ -1,26 +1,33 @@
+using GUZ.Core.Config;
+#if !UNITY_EDITOR
 using System;
 using System.IO;
-using GUZ.Core.Config;
+using System.Linq;
+using UberLogger;
 using UnityEngine;
+using ILogger = UberLogger.ILogger;
+using Object = UnityEngine.Object;
+#endif
 
 namespace GUZ.Core.Util
 {
     public class FileLoggingHandler
     {
-        private StreamWriter _fileWriter;
-        private LogLevel _logLevel;
+#if UNITY_EDITOR
+        public void Init(JsonRootConfig _)
+        {
+            // Disabled in Editor.
+        }
+
+        public void Destroy()
+        {
+            // Disabled in Editor.
+        }
+#else
+        private FileLoggingLogger _logger;
 
         private const string _logFileName = "Gothic-UnZENity.log.txt";
         
-        // Levels are aligned with Unity's levels: UnityEngine.LogType
-        private enum LogLevel
-        {
-            Debug = 0,
-            Warning = 1,
-            Error = 2,
-            Exception = 3
-        }
-
         public FileLoggingHandler()
         {
             // As we need to get logs as early as possible, we will pre-initialize it immediately.
@@ -29,8 +36,6 @@ namespace GUZ.Core.Util
 
         private void PreInit()
         {
-            Application.logMessageReceived += HandleLog;
-
             string rootFolder;
             if (Application.platform == RuntimePlatform.Android)
             {
@@ -40,72 +45,143 @@ namespace GUZ.Core.Util
             {
                 rootFolder = Application.persistentDataPath;
             }
-            
-            _fileWriter = new StreamWriter($"{rootFolder}/{_logFileName}", false);
-            _fileWriter.WriteLine("DeviceModel: " + SystemInfo.deviceModel);
-            _fileWriter.WriteLine("DeviceType: " + SystemInfo.deviceType);
-            _fileWriter.WriteLine("OperatingSystem: " + SystemInfo.operatingSystem);
-            _fileWriter.WriteLine("OperatingSystemFamily: " + SystemInfo.operatingSystemFamily);
-            _fileWriter.WriteLine("MemorySize: " + SystemInfo.systemMemorySize);
-            _fileWriter.WriteLine("Gothic-UnZENity Version: " + Application.version);
-            _fileWriter.WriteLine();
-            _fileWriter.Flush();
+
+            _logger = new FileLoggingLogger($"{rootFolder}/{_logFileName}", false);
+            UberLogger.Logger.AddLogger(_logger);
+
+            _logger.WriteLine("DeviceModel: " + SystemInfo.deviceModel);
+            _logger.WriteLine("DeviceType: " + SystemInfo.deviceType);
+            _logger.WriteLine("OperatingSystem: " + SystemInfo.operatingSystem);
+            _logger.WriteLine("OperatingSystemFamily: " + SystemInfo.operatingSystemFamily);
+            _logger.WriteLine("MemorySize: " + SystemInfo.systemMemorySize);
+            _logger.WriteLine("Gothic-UnZENity Version: " + Application.version);
+            _logger.WriteLine(string.Empty);
         }
 
-        public void Init(JsonRootConfig rootRootConfig)
+        public void Init(JsonRootConfig rootConfig)
         {
-            if (Enum.TryParse(rootRootConfig.LogLevel, true, out LogLevel value))
+            LogSeverity logLevel;
+
+            if (Enum.TryParse(rootConfig.LogLevel, true, out LogSeverity value))
             {
-                _fileWriter.WriteLine("LogLevel Setting found: " + rootRootConfig.LogLevel);
-                _logLevel = value;
+                _logger.WriteLine("LogLevel Setting found: " + rootConfig.LogLevel);
+                logLevel = value;
             }
             else
             {
-                _fileWriter.WriteLine("LogLevel Setting not found. Setting Default to >Warning<.");
-                _logLevel = LogLevel.Warning;
-            }
-            
-            _fileWriter.Flush();
-        }
-
-        private void HandleLog(string logString, string stackTrace, LogType type)
-        {
-            if (!ShallBeLogged(type))
-            {
-                return;
+                _logger.WriteLine("LogLevel Setting not found. Setting Default to >Warning<.");
+                logLevel = LogSeverity.Warning;
             }
 
-            _fileWriter.WriteLine(type + ": " + logString);
-            
-            if (type == LogType.Exception)
+            string[] logCategories;
+            if (rootConfig.LogCategories != null)
             {
-                _fileWriter.WriteLine(stackTrace);
+                logCategories = rootConfig.LogCategories.Split(',', ';')
+                    // We sanitize possible case issues as we want to have string channels for a small performance benefit when logging at runtime.
+                    .Select(i => Enum.TryParse(i.Trim(), true, out LogCat cat) ? cat.ToString() : null)
+                    .Where(i => i != null)
+                    .ToArray();
             }
-            
-            _fileWriter.Flush();
+            else
+            {
+                logCategories = Array.Empty<string>();
+            }
+
+            if (logCategories.Length == 0)
+            {
+                var enumValues = Enum.GetValues(typeof(LogCat)).Cast<LogCat>().Select(v => v.ToString());
+                _logger.WriteLine($"No valid category found. Activating logs for all categories: [{string.Join(';', enumValues)}]");
+            }
+            else
+            {
+                _logger.WriteLine($"Log Categories found and activated: [{string.Join(';', logCategories)}]");
+            }
+            _logger.WriteLine(string.Empty);
+
+            UberLogger.Logger.AddFilter(new FileLoggingFilter(logLevel, logCategories));
         }
 
-        private bool ShallBeLogged(LogType type)
-        {
-            switch (_logLevel)
-            {
-                case LogLevel.Debug:
-                    return true; // Log everything
-                case LogLevel.Warning:
-                    return type != LogType.Log; // includes Assert, Warning, Error, Exception
-                case LogLevel.Error:
-                    return type == LogType.Error || type == LogType.Exception;
-                case LogLevel.Exception:
-                    return type == LogType.Exception;
-                default:
-                    return true; // never reached, but compiler demands it.
-            }
-        }
-        
         public void Destroy()
         {
-            Application.logMessageReceived -= HandleLog;
-            _fileWriter.Close();
+            _logger = null;
         }
+
+
+        private class FileLoggingLogger : ILogger
+        {
+            private readonly StreamWriter _logFileWriter;
+            private readonly bool _includeCallStacks;
+
+            public FileLoggingLogger(string fileLogPath, bool includeCallStacks)
+            {
+                _includeCallStacks = includeCallStacks;
+                _logFileWriter = new StreamWriter(fileLogPath, false);
+                _logFileWriter.AutoFlush = true;
+            }
+
+            public void Log(LogInfo logInfo)
+            {
+                lock(this)
+                {
+                    // RFC 5424: <timestamp> <severity> [<category>] <message>
+                    var fullMessage = string.Format("{0} {1} [{2}] {3}",
+                        logInfo.GetRelativeTimeStampAsString(),
+                        logInfo.Severity,
+                        logInfo.Channel,
+                        logInfo.Message
+                    );
+
+                    _logFileWriter.WriteLine(fullMessage);
+
+                    if(_includeCallStacks && logInfo.Callstack.Count>0)
+                    {
+                        foreach(var frame in logInfo.Callstack)
+                        {
+                            _logFileWriter.WriteLine(frame.GetFormattedMethodNameWithFileName());
+                        }
+                        _logFileWriter.WriteLine();
+                    }
+                }
+            }
+
+            /// <summary>
+            /// Write a single line without additional formatting or checks.
+            /// </summary>
+            public void WriteLine(string message)
+            {
+                lock(this)
+                {
+                    _logFileWriter.WriteLine(message);
+                }
+            }
+
+            ~FileLoggingLogger()
+            {
+                lock (this)
+                {
+                    _logFileWriter.Close();
+                }
+            }
+        }
+
+        private class FileLoggingFilter : IFilter
+        {
+            private readonly LogSeverity _logLevel;
+            private readonly string[] _logCategories;
+
+
+            public FileLoggingFilter(LogSeverity logLevel, string[] logCategories)
+            {
+                _logLevel = logLevel;
+                _logCategories = logCategories;
+            }
+
+            public bool ApplyFilter(string channel, Object source, LogSeverity severity, object message, params object[] par)
+            {
+                return severity >= _logLevel
+                       && (_logCategories.Length == 0 || _logCategories.Contains(channel));
+            }
+        }
+#endif
     }
 }
