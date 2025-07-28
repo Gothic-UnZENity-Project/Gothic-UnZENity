fc298ccb17899cadcd53a708f4a168509461d427
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2025-07-29 00:13:04 +0200

    feat: Optimize logging format and category button view.

--- Assets/UnZENity-VR/Scripts/Components/Marvin/MarvinLogsTabHandler.cs
+++ Assets/UnZENity-VR/Scripts/Components/Marvin/MarvinLogsTabHandler.cs
@@ -1,9 +1,10 @@
 using System;
-using System.Collections;
+using System.Linq;
 using GUZ.Core;
 using GUZ.Core.Util;
 using TMPro;
 using UberLogger;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 using ILogger = UberLogger.ILogger;
@@ -15,8 +16,6 @@ namespace GUZ.VR.Components.Marvin
     public class MarvinLogsTabHandler : MonoBehaviour, ILogger
     {
         [SerializeField] private RectTransform _categoriesRoot;
-        [SerializeField] private RectTransform _categoryContentContainer;
-
         
         [SerializeField] private ScrollRect _logsRoot;
         [SerializeField] private RectTransform _logContentContainer;
@@ -25,7 +24,6 @@ namespace GUZ.VR.Components.Marvin
         [SerializeField] private Texture2D _warningIcon;
         [SerializeField] private Texture2D _errorIcon;
         
-        private const int _paddingTop = 10;
         private const int _logLineHeight = 15;
         private int _logCount;
         
@@ -52,10 +50,10 @@ namespace GUZ.VR.Components.Marvin
         /// We need to disable Logging (and especially creating new GameObjects) when we recompile at runtime (Editor only).
         /// Otherwise, Unity will crash.
         /// </summary>
-        [UnityEditor.InitializeOnLoadMethod]
+        [InitializeOnLoadMethod]
         static void InitializeEditor()
         {
-            UnityEditor.AssemblyReloadEvents.beforeAssemblyReload += () =>
+            AssemblyReloadEvents.beforeAssemblyReload += () =>
             {
                 // Find and remove this logger from UberLogger
                 Logger.RemoveLogger(typeof(MarvinLogsTabHandler).FullName);
@@ -65,20 +63,24 @@ namespace GUZ.VR.Components.Marvin
 
         private void FillCategories()
         {
+            var defaultCategories = new[] { "Nothing", "Everything", "-No Channel-" };
+            
             var catIndex = 0;
-            foreach (var category in Enum.GetNames(typeof(LogCat)))
+            foreach (var category in defaultCategories.Concat(Enum.GetNames(typeof(LogCat))))
             {
-                var categoryGo = ResourceLoader.TryGetPrefabObject(PrefabType.UiDebugButton, name: category, parent: _categoryContentContainer.gameObject);
-                var categoryTransform = categoryGo.GetComponent<RectTransform>();
-                categoryTransform.localPosition = new Vector2(0, -_paddingTop - (_logLineHeight / 2f) - _logLineHeight * 2 * catIndex);
-                categoryTransform.sizeDelta = new Vector2(_categoriesRoot.rect.width, _logLineHeight * 2);
+                var categoryGo = ResourceLoader.TryGetPrefabObject(PrefabType.UiDebugButton, name: category, parent: _categoriesRoot.gameObject);
+                var categoryTransform = categoryGo!.GetComponent<RectTransform>();
+                categoryTransform.localPosition = new Vector2(0, -(_logLineHeight / 2f) - _logLineHeight * catIndex);
+                categoryTransform.sizeDelta = new Vector2(_categoriesRoot.rect.width - 25f, _logLineHeight);
                 
                 var textComp =  categoryGo.GetComponentInChildren<TMP_Text>();
                 textComp.text = category;
                 catIndex++;
-            }
 
-            _categoryContentContainer.sizeDelta = new Vector2(_categoryContentContainer.sizeDelta.x, _paddingTop + (_logLineHeight / 2f) + _logLineHeight * 2 * catIndex);
+                // One empty line to separate "special" categories from others.
+                if (category == "-No Channel-")
+                    catIndex++;
+            }
         }
 
         public void Log(LogInfo logInfo)
@@ -95,11 +97,13 @@ namespace GUZ.VR.Components.Marvin
             itemTransform.anchorMax = new Vector2(0, 1);
             // Offset: Half of text height - itemIndex*itemHeight
             itemTransform.localPosition = new Vector2(0, -(_logLineHeight / 2f) - _logLineHeight * _logCount);
+
+            var message = FormatLogText(logInfo);
             
             var textComp = itemGo.GetComponentInChildren<TMP_Text>();
             var textRectComp = textComp.GetComponent<RectTransform>();
-            var preferredSize = textComp.GetPreferredValues(logInfo.Message); // Calculate size for message based on 
-            textComp.text = logInfo.Message;
+            var preferredSize = textComp.GetPreferredValues(message); // Calculate size for message based on 
+            textComp.text = message;
             textRectComp.sizeDelta = new Vector2(preferredSize.x, _logLineHeight);
             textRectComp.localPosition = new Vector2(preferredSize.x / 2, 0);
 
@@ -121,6 +125,30 @@ namespace GUZ.VR.Components.Marvin
             // Height: Half of text height (for offset) + itemCount*itemHeight
             _logContentContainer.sizeDelta = new Vector2(_logContentContainer.sizeDelta.x, (_logLineHeight / 2f) + (_logCount + 1) * _logLineHeight);
             _logCount++;
+        }
+
+        /// <summary>
+        /// Format the message as follows:
+        ///     [channel] 0.000 : message  <-- Both channel and time shown
+        ///     0.000 : message            <-- Time shown, channel hidden
+        ///     [channel] : message        <-- Channel shown, time hidden
+        ///     message                    <-- Both channel and time hidden
+        /// </summary>
+        private string FormatLogText(LogInfo log)
+        {
+            var showChannel = !string.IsNullOrEmpty(log.Channel);
+            var channelMessage = showChannel ? string.Format("[{0}]", log.Channel) : "";
+            var channelTimeSeparator = showChannel ? " " : "";
+            var timeMessage = string.Format("{0}", log.GetRelativeTimeStampAsString());
+            var prefixMessageSeparator = " : ";
+            
+            return string.Format("{0}{1}{2}{3}{4}",
+                channelMessage,
+                channelTimeSeparator,
+                timeMessage,
+                prefixMessageSeparator,
+                log.Message
+            );
         }
     }
 }
