6cf8bcdfeec60ed066b5506f99596ff59e69b44c
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2025-10-10 22:43:44 +0200

    feat: Implement Status bar prefabs (Health+Mana+Misc) and add Misc (air) bar on VR player.

---
+++ Assets/UnZENity-Core/Scripts/Adapters/UI/StatusBars/StatusBarAdapter.cs
@@ -0,0 +1,56 @@
+using System;
+using GUZ.Core.Services.Meshes;
+using Reflex.Attributes;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GUZ.Core.Adapters.UI.StatusBars
+{
+    public class StatusBarAdapter : MonoBehaviour
+    {
+        [SerializeField] private StatusType _statusType;
+        [SerializeField] private Image _background;
+        [SerializeField] private Image _statusValue;
+
+        [Inject] private readonly TextureService _textureService;
+
+        private enum StatusType
+        {
+            Health,
+            Mana,
+            Misc
+        }
+
+        private void Start()
+        {
+            DisableBar();
+            GlobalEventDispatcher.ZenKitBootstrapped.AddListener(StartDelayed);
+        }
+
+        private void StartDelayed()
+        {
+            _background.material = _textureService.StatusBarBackgroundMaterial;
+
+            _statusValue.fillAmount = 1f;
+            _statusValue.material = _statusType switch
+            {
+                StatusType.Health => _textureService.StatusBarHealthMaterial,
+                StatusType.Mana => _textureService.StatusBarManaMaterial,
+                StatusType.Misc => _textureService.StatusBarMiscMaterial,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+
+        public void DisableBar()
+        {
+            _background.enabled = false;
+            _statusValue.enabled = false;
+        }
+
+        public void EnableBar()
+        {
+            _background.enabled = true;
+            _statusValue.enabled = true;
+        }
+    }
+}
